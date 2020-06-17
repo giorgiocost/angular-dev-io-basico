aac1cda0a2083026858b2f3c1ab9468ebe2f2785
Giorgio Costa <gegiorgio1991@gmail.com>
2020-06-16 22:16:51 -0300

    configuração de rotas

--- MeuProjeto/src/app/app.module.ts
+++ MeuProjeto/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,16 +6,22 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    ContatoComponent,
+    SobreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash: true})],
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ MeuProjeto/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { HomeComponent } from './navegacao/home/home.component';
+
+
+export const rootRouterConfig: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
+    { path: 'contato', component: ContatoComponent},
+    { path: 'sobre', component: SobreComponent},
+];
