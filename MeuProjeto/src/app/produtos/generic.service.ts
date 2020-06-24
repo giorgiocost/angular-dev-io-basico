a70b01918000551e5d56f75259189028afb73fef
Giorgio Costa <gegiorgio1991@gmail.com>
2020-06-24 00:48:30 -0300

    removido comentários e importes desnecessários

--- MeuProjeto/src/app/produtos/generic.service.ts
+++ MeuProjeto/src/app/produtos/generic.service.ts
@@ -1,14 +1,12 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Produto } from './produto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenericService {
 
-  // protected op = "http://localhost:3000/produtos";
   constructor(private http: HttpClient){}
   
   appRequest(request: any): Observable<any> {
@@ -17,7 +15,4 @@ export class GenericService {
     }
   }
 
-  // obterProdutos(): Observable<Produto[]> {
-    
-  // }
 }
