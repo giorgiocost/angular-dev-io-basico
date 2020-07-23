572a43266f6d2b484ec4421b6cf13064184227ed
giorgiocost <gegiorgio1991@gmail.com>
2020-07-22 22:53:12 -0300

    modulo compartilhado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
+// import { MenuComponent } from './navegacao/menu/menu.component';
+// import { HomeComponent } from './navegacao/home/home.component';
+// import { FooterComponent } from './navegacao/footer/footer.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { rootRouterConfig } from './app.routes';
@@ -17,20 +17,22 @@ import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.compo
 import { NgBrazil } from 'ng-brazil';
 import { TextMaskModule } from 'angular2-text-mask';
 import { CustomFormsModule } from 'ng2-validation';
+import { NavegacaoModule } from './navegacao/navegacao.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
+    // MenuComponent,
+    // HomeComponent,
+    // FooterComponent,
     ContatoComponent,
     SobreComponent,
     DataBindingComponent,
     CadastroComponent,
   ],
   imports: [
+    NavegacaoModule,
     HttpClientModule,
     BrowserModule,
     FormsModule,
