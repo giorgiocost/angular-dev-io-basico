7622f45a9249f2a43074980a32b70f4fdcec56e3
giorgiocost <gegiorgio1991@gmail.com>
2020-07-23 23:27:47 -0300

    modulo de roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
-import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
-// import { MenuComponent } from './navegacao/menu/menu.component';
-// import { HomeComponent } from './navegacao/home/home.component';
-// import { FooterComponent } from './navegacao/footer/footer.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
-import { rootRouterConfig } from './app.routes';
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 import { ProdutosService } from './produtos/produtos.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -18,14 +13,12 @@ import { NgBrazil } from 'ng-brazil';
 import { TextMaskModule } from 'angular2-text-mask';
 import { CustomFormsModule } from 'ng2-validation';
 import { NavegacaoModule } from './navegacao/navegacao.module';
+import { AppRoutingModule } from './app.routes';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    // MenuComponent,
-    // HomeComponent,
-    // FooterComponent,
     ContatoComponent,
     SobreComponent,
     DataBindingComponent,
@@ -40,7 +33,7 @@ import { NavegacaoModule } from './navegacao/navegacao.module';
     TextMaskModule,
     NgBrazil,
     CustomFormsModule,
-    [RouterModule.forRoot(rootRouterConfig, {useHash: true})],
+    AppRoutingModule,
   ],
   providers: [
     ProdutosService,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
+import { NgModule } from '@angular/core';
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 
-export const rootRouterConfig: Routes = [
+const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent},
     { path: 'contato', component: ContatoComponent},
@@ -13,3 +14,14 @@ export const rootRouterConfig: Routes = [
     { path: 'feature-data-binding', component: DataBindingComponent},
     { path: 'cadastro', component: CadastroComponent},
 ];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(rootRouterConfig, { useHash: true }),
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule{}
