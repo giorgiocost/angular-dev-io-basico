99bd2ae23a153c307d8d61d52ed210ea175384b8
giorgiocost <gegiorgio1991@gmail.com>
2020-07-26 22:30:22 -0300

    pipe moeda pt-br

--- src/app/demos/arquitetura-componentes/produto.module.ts
+++ src/app/demos/arquitetura-componentes/produto.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProdutoDashboardComponent } from './produto-dashboard/produto-dashboard.component';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 @NgModule({
     declarations: [
