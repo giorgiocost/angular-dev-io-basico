2d249fc57e61e1749c55ab1124a128a422ade3fb
giorgiocost <gegiorgio1991@gmail.com>
2020-08-13 23:17:20 -0300

    view editar produto

--- src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
+++ src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProdutosService } from 'src/app/produtos/produtos.service';
+import { Produto } from 'src/app/produtos/produto';
 
 @Component({
   selector: 'app-editar-produto',
@@ -9,11 +10,15 @@ import { ProdutosService } from 'src/app/produtos/produtos.service';
 })
 export class EditarProdutoComponent implements OnInit {
 
+  produto: Produto;
+
   constructor(private router: ActivatedRoute, private produtosService: ProdutosService) { }
 
   ngOnInit(): void {
     this.router.params.subscribe(params => {
-      this.produtosService.obterProdutoPorId(params['id']).subscribe(data => console.log('produto', data))
+      this.produtosService.obterProdutoPorId(params['id']).subscribe(data => {
+        this.produto = data;
+      });
     });
   }
 
