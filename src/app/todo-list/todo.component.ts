c8385e08c16c949d2fecd0841dd48737e4548c9d
giorgiocost <gegiorgio1991@gmail.com>
2020-09-02 23:43:52 -0300

    removido todo db e propriedade get service

--- src/app/todo-list/todo.component.ts
+++ src/app/todo-list/todo.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
-  templateUrl: './todo.component.html'
+  templateUrl: './todo.component.html',
+  styles: [
+    `
+      .container {
+        margin-top: 72px
+      }
+    `
+  ]
 })
 export class TodoComponent {}
--- src/app/todo-list/todo.service.ts
+++ src/app/todo-list/todo.service.ts
@@ -1,4 +1,16 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from './task';
 
 @Injectable()
-export class TasksService { }
+export class TasksService { 
+    url = 'http://localhost:3000/todolist';
+    constructor(private http: HttpClient) { }
+
+    getToDoList$: Observable<Task[]> = this.http.get<Task[]>(this.url);
+
+    // getToDoList() : Observable<Task[]> {
+    //     return this.http.get<Task[]>(this.url);
+    // }
+}
