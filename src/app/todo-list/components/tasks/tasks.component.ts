f4f4745f08c530f5aae2502a11bd70a3fa027175
giorgiocost <gegiorgio1991@gmail.com>
2020-09-08 23:28:49 -0300

    lista de tarefas pra fazer

--- src/app/todo-list/components/tasks/tasks.component.ts
+++ src/app/todo-list/components/tasks/tasks.component.ts
@@ -1,18 +1,30 @@
-import { Observable } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Store } from './../../todo.store';
+import { Observable, Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TasksService } from '../../todo.service';
 
 @Component({
   selector: 'tasks',
   templateUrl: './tasks.component.html'
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, OnDestroy {
   
   todolist$: Observable<any>;
+  subscription: Subscription;
 
-  constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService, private store: Store) {}
 
   ngOnInit() { 
-    this.todolist$ = this.tasksService.getToDoList$;
-  }  
+    this.todolist$ = this.store.getTodoList()
+      .pipe(
+        map(todolist => todolist.filter(task => !task.iniciado && !task.finalizado))
+      )
+      this.subscription = this.tasksService.getToDoList$
+        .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
